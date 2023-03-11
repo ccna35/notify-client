3eb051b6c27acfb30e4dbf94d492a3d3714ed36a
ccna35 <shawkykhalil@gmail.com>
2023-03-11 17:51:40 +0200

    "Added routes to the landing page"

--- src/Layouts/LandingPageLayout.tsx
+++ src/Layouts/LandingPageLayout.tsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 
 export default function LandingPageLayout() {
   return (
     <div>
       <nav className="p-4 bg-slate-500 flex gap-4">
-        <span>Menu Item</span>
-        <span>Menu Item</span>
-        <span>Menu Item</span>
-        <span>Menu Item</span>
+        <Link to="/register" className="font-medium">
+          Register
+        </Link>
+        <Link to="/login" className="font-medium">
+          Login
+        </Link>
+        <span className="font-medium">Menu Item</span>
+        <span className="font-medium">Menu Item</span>
+        <span className="font-medium">Menu Item</span>
       </nav>
       <Outlet />
       <footer>

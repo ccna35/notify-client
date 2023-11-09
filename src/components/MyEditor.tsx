f6ce9fd5f1aa021ea05d7ee3c844f0827a05fdac
Shawky Khalil <shawkykhalil@gmail.com>
2023-11-09 19:31:01 +0200

    FIX some issues

--- src/components/MyEditor.tsx
+++ src/components/MyEditor.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
 
 export default function MyEditor() {
   const [value, setValue] = useState("");
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  // return <ReactQuill theme="snow" value={value} onChange={setValue} />;
 }
